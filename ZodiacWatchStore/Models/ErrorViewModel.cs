903fe439a068b8fd5b077f0a55b754497ab55e83
mustafahmadov <mustafaia@code.edu.az>
2021-02-23 12:31:05 +0400

    Connection String created

---
+++ ZodiacWatchStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZodiacWatchStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
