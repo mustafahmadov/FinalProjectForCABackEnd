6540f7dbf238d63b2a963127dc35aee18187fd3e
mustafahmadov <mustafaia@code.edu.az>
2021-02-24 01:08:48 +0400

    Product simple view component added

---
+++ ZodiacWatchStore/ViewComponents/ProductSimpleViewComponent.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZodiacWatchStore.DAL;
+using ZodiacWatchStore.Models;
+
+namespace ZodiacWatchStore.ViewComponents
+{
+    public class ProductSimpleViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public ProductSimpleViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(string filterFor)
+        {
+            List<Product> products = new List<Product>();
+            if (filterFor == "mostviewed")
+            {
+                products = _context.Products.Where(p => p.HasDeleted == false)
+                                                  .OrderByDescending(p => p.ViewCount).Take(3).ToList();
+            }
+            else if (filterFor == "countdes")
+            {
+                products = _context.Products.Where(p => p.HasDeleted == false)
+                                                  .OrderBy(p=>p.Count).Take(3).ToList();
+            }
+            else if (filterFor == "discountproducts")
+            {
+                products = _context.Products.Where(p => p.HasDeleted == false && p.Discount > 0)
+                                                            .Take(3).ToList();
+            }
+            return View(await Task.FromResult(products));
+        }
+    }
+}
